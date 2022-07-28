8cba9459696d13c954e7b5689314611602b5a928
gangbro-dev <gangbro.dev@gmail.com>
2022-07-28 23:02:15 +0900

    20220728 night

--- BackJoon/1260.py
+++ BackJoon/1260.py
@@ -1,22 +1,25 @@
-def DFS(graph_list, sol_list, start):
-    for i in graph_list[start]:
+def DFS(graph_dict, sol_list, start):
+    for i in graph_dict[start]:
         if not i in sol_list:
             sol_list.append(i)
             num = i
-            DFS(graph_list, sol_list, num)
+            DFS(graph_dict, sol_list, num)
     return ' '.join(list(map(str, sol_list)))
 
-def BFS(graph_list, sol_list, start):
-    count = 1
-    while True:
-        for i in graph_list[start]:
-            if not i in sol_list:
+def BFS(graph_dict, sol_list, start):
+    cnt = 0
+    backcnt = -1
+    while len(sol_list) < graph_dict['N'][0]:
+        if backcnt == cnt:
+            break
+        for i in graph_dict[sol_list[cnt]]:
+            if i not in sol_list:
                 sol_list.append(i)
-        if len(sol_list) == graph_list['N'][0]:
-            return ' '.join(list(map(str, sol_list)))
-        else:
-            start = count
-            count += 1
+        backcnt = len(sol_list)
+        cnt += 1
+        
+        
+    return ' '.join(list(map(str, sol_list)))
                   
 complete_set = set() # 탐색 완료한 정점 모음
 setting_value = input() # 첫줄 그래프 정보 받음
@@ -24,7 +27,7 @@ setting_value = input() # 첫줄 그래프 정보 받음
 N, M, start = map(int, setting_value.split())
 
 conn_info = {'N' : [N]}
-
+conn_info[start] = []
 # 각 노드마다 연결정보를 conn_info 딕셔너리에 저장
 for i in range(M):
     conn_value = list(map(int, input().split()))
@@ -39,9 +42,7 @@ for i in range(M):
 
 for i in conn_info.keys(): # 각 노드별 연결정보 정렬
         conn_info[i].sort()
-
 solution = [start]
-#print(DFS(conn_info, solution, start))
+print(DFS(conn_info, solution, start))
 solution = [start]
 print(BFS(conn_info, solution, start))
-
