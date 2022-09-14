a92925a196ee8c15afb916e7d26c8e2b87b20f75
gangbro-dev <gangbro.dev@gmail.com>
2022-09-14 20:10:33 +0900

    20220914

---
+++ BackJoon/11779.py
@@ -0,0 +1,42 @@
+from sys import stdin
+import heapq
+
+N = int(stdin.readline())
+M = int(stdin.readline())
+bus = [tuple(map(int, stdin.readline().split())) for _ in range(M)]
+start, end = map(int, stdin.readline().split())
+
+adj_graph = [dict() for _ in range(N+1)]
+for line in bus:
+    # 노선 정보를 저장한다. 만약 자기자신으로 오는 노선이 있다면, 무시한다.
+    if line[0] == line[1]:
+        continue
+    # 아직 저장되지 않은 노선이라면 추가한다.
+    if line[1] not in adj_graph[line[0]].keys():
+        adj_graph[line[0]][line[1]] = line[2]
+    # 겹치는 노선이라면, 비용이 작을 때, 저장한다.
+    elif adj_graph[line[0]][line[1]] > line[2]:
+        adj_graph[line[0]][line[1]] = line[2]
+
+cost_table = [100001 * N] * (N+1)               # 각 노드로 가는 최소비용
+cost_table[start] = 0                           # 시작점에서 시작점으로 가는 비용은 0
+q = list()
+heapq.heappush(q, (0, start))
+route = [list() for _ in range(N+1)]            # 노드 진행 정보를 저장할 리스트
+route[start] = [start]
+
+while q:
+    dist, now = heapq.heappop(q)
+    if cost_table[now] < dist:
+        continue
+    # 현재 노드까지 도착하는 최소 비용 + 간선으로 인한 비용 = 이 노드에서 다음노드로 가는 비용
+    for node, cost in adj_graph[now].items():
+        if cost_table[now] + cost < cost_table[node]:
+            cost_table[node] = cost_table[now] + cost
+            heapq.heappush(q, (cost_table[node], node))
+            route[node] = route[now] + [node]
+
+
+print(cost_table[end])
+print(len(route[end]))
+print(*route[end])
--- BackJoon/1446.py
+++ BackJoon/1446.py
@@ -1,30 +1,56 @@
+# 부분집합을 이용한 풀이 -> 반례찾기 실패
+# N, D = map(int, input().split())
+# from itertools import combinations as combi
+# short_cut = list()
+# for _ in range(N):
+#     a = tuple(map(int, input().split()))
+#     if a[1] <= D and a[1] - a[0] > a[2]:        # 유효한 지름길만 사용
+#         short_cut.append(a)
+#
+# short_cut_len = []                              # 유효한 지름길 조합의 단축 길이 리스트
+# for i in short_cut:
+#     short_cut_len.append(i[1]-i[0]-i[2])        # 단축 길이 = 도착점 - 출발점 - 지름길 길이
+#
+# for i in range(2, N+1):                         # 원소가 2개 이상인 조합에 대해서
+#     for j in combi(short_cut, i):
+#         flag = False                            # 유효한 조합인지 확인(출발점, 도착점을 확인해서 겹치는 구간인지 확인)
+#         for k in range(len(j)):
+#             for l in range(k+1, len(j)):
+#                 if j[k][0] <= j[l][0] < j[k][1] or j[k][0] < j[l][1] <= j[k][1]:
+#                     flag = True
+#                     break
+#             if flag:
+#                 break
+#         else:                                   # 겹치지 않는다면, 조합 내부의 총 줄어드는 길이를 저장
+#             temp = 0
+#             for x in j:
+#                 temp += x[1]-x[0]-x[2]
+#             short_cut_len.append(temp)
+#
+# best = max(short_cut_len)
+# print(D - best)
+
+# 2. 다익스트라 도전!
 N, D = map(int, input().split())
-from itertools import combinations as combi
-short_cut = list()
+# 기본적인 거리로 테이블 초기화
+cost_table = [x for x in range(D + 1)]
+# 지름길 입력
+short_cut_list = list()
 for _ in range(N):
     a = tuple(map(int, input().split()))
     if a[1] <= D and a[1] - a[0] > a[2]:        # 유효한 지름길만 사용
-        short_cut.append(a)
+        short_cut_list.append(a)
+# 지름길 도착점이 가장 앞쪽인 순으로 정렬
+short_cut_list.sort(key=lambda x: x[1])
 
-short_cut_len = []                              # 유효한 지름길 조합의 단축 길이 리스트
-for i in short_cut:
-    short_cut_len.append(i[1]-i[0]-i[2])        # 단축 길이 = 도착점 - 출발점 - 지름길 길이
+for short_cut in short_cut_list:
+    # 지름길 도착점까지의 비용이 새로운 최소비용이라면 갱신
+    if cost_table[short_cut[1]] > cost_table[short_cut[0]] + short_cut[2]:
+        cost_table[short_cut[1]] = cost_table[short_cut[0]] + short_cut[2]
+        # 갱신한 최소비용 값을 이용해서 모든 다음 지점들의 비용 갱신
+        for x in range(short_cut[1], D):
+            cost_table[x+1] = cost_table[x] + 1
 
-for i in range(2, N+1):                         # 원소가 2개 이상인 조합에 대해서
-    for j in combi(short_cut, i):
-        flag = False                            # 원소끼리 겹치는 조합인가?
-        for k in range(len(j)):
-            for l in range(k+1, len(j)):
-                if j[k][0] <= j[l][0] < j[k][1] or j[k][0] < j[l][1] <= j[k][1]:
-                    flag = True
-                    break
-            if flag:
-                break
-        else:                                   # 겹치지 않는다면, 조합 내부의 총 줄어드는 길이를 저장
-            temp = 0
-            for x in j:
-                temp += x[1]-x[0]-x[2]
-            short_cut_len.append(temp)
+print(cost_table[-1])
 
-best = max(short_cut_len)
-print(D - best)
+
