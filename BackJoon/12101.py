ee698e58045d963dd2e527aa50f262323e370286
gangbro-dev <gangbro.dev@gmail.com>
2022-10-17 23:08:39 +0900

    20221017;

---
+++ BackJoon/12101.py
@@ -0,0 +1,26 @@
+ans = list()
+
+
+def make_eq(left, length, eq=''):
+    global ans
+    if len(ans) == length:
+        return
+    if left == 0:
+        ans.append(eq[1:])
+        return
+    elif left >= 3:
+        make_eq(left - 1, length, eq + '+1')
+        make_eq(left - 2, length, eq + '+2')
+        make_eq(left - 3, length, eq + '+3')
+    elif left == 2:
+        make_eq(left - 1, length, eq + '+1')
+        make_eq(left - 2, length, eq + '+2')
+    else:
+        make_eq(left - 1, length, eq + '+1')
+
+
+n, k = map(int, input().split())
+make_eq(n, k)
+if len(ans) < k:
+    ans.append(-1)
+print(ans[-1])
